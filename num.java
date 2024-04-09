33e86dbf883b59e7d5e71b6731e90c060521576a
HariHaran <haranhari99815@gmail.com>
2024-04-09 13:08:16 +0530

    have added reverse the string code snippet

--- num.java
+++ num.java
@@ -1,63 +1,26 @@
-import java.util.Scanner;
+ public class Num{
+public static void main(String args[]){
+String name="hariharan";
+boolean false_palin=true;
+for(int i=0;i<name.length()/2;i++){
+if(name.charAt(1)!=name.charAt(name.length() - i - 1)){
+false_palin=false;
+break;
+}
+}
+if(false_palin){
+System.out.println(name+"is palindrome");
+}
+else{
 
-public class Main {
-    // Function to check if a number is Armstrong
-    static boolean isArmstrong(int num) {
-        int originalNum, remainder, result = 0, n = 0;
+System.out.println(name+"is not palindromec");}
 
-        originalNum = num;
+System.out.println("starting with reversing the string");
+String full_name="hariharan",reverse="",temp;
+for(int i=full_name.length()-1;i>=0;i--){
+reverse+=full_name.charAt(i);
 
-        // Store the number of digits of num
-        while (originalNum != 0) {
-            originalNum /= 10;
-            ++n;
-        }
-
-        originalNum = num;
-
-        // Calculate Armstrong value
-        while (originalNum != 0) {
-            remainder = originalNum % 10;
-            result += Math.pow(remainder, n);
-            originalNum /= 10;
-        }
-
-        // Check if the number is Armstrong
-        return result == num;
-    }
-
-    // Function to check if a number is Palindrome
-    static boolean isPalindrome(int num) {
-        int reversedNum = 0, remainder, originalNum;
-
-        originalNum = num;
-
-        // Reverse the number
-        while (num != 0) {
-            remainder = num % 10;
-            reversedNum = reversedNum * 10 + remainder;
-            num /= 10;
-        }
-
-        // Check if the number is palindrome
-        return originalNum == reversedNum;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-
-        if (isArmstrong(number)) {
-            System.out.println(number + " is an Armstrong number.");
-        } else {
-            System.out.println(number + " is not an Armstrong number.");
-        }
-
-        if (isPalindrome(number)) {
-            System.out.println(number + " is a Palindrome number.");
-        } else {
-            System.out.println(number + " is not a Palindrome number.");
-        }
-    }
+}System.out.println(full_name+"before reversing");
+System.out.println(reverse+"after reversing");
+}
 }
