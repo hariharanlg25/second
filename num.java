1455a036fb5a2374e00c1603c4937d573c9a3575
HariHaran <haranhari99815@gmail.com>
2024-04-09 13:23:42 +0530

    have added the swap two numbers code snippet

--- num.java
+++ num.java
@@ -12,16 +12,16 @@ if(false_palin){
 System.out.println(name+"is palindrome");
 }
 else{
-// *******************************************
 System.out.println(name+"is not palindromec");}
+// *******************************************
 
 System.out.println("starting with reversing the string");
 String full_name="hariharan",reverse="",temp;
 for(int i=full_name.length()-1;i>=0;i--){
 reverse+=full_name.charAt(i);
 
-}System.out.println(full_name+"before reversing");
-System.out.println(reverse+"after reversing");
+}System.out.println(full_name+" before reversing");
+System.out.println(reverse+" after reversing");
 // *******************************************
 System.out.println("fibonacci series");
 int a=0,b=1,c,num=9	;
@@ -32,6 +32,22 @@ a=b;
 b=c;
 
 }
+
+
+// *******************************************
+
+System.out.println(" program to swap ");
+
+int first=18;
+int second=89;
+int dummy=0;
+
+System.out.println(first+"before"+second+"before");
+
+dummy=first;
+first=second;
+second=dummy;
+System.out.println(first+"after"+second+"after");
 
 }
 }
